33b9f90f00b697d33381ea58fe22bddc6ee68bf5
Tyler Nguyen <38928951+IWantToHelp@users.noreply.github.com>
2023-11-28 11:24:53 -0700

    Cleaned up keyboard_control.m for readability

--- keyboard_control.m
+++ keyboard_control.m
@@ -19,32 +19,11 @@ while 1
             %disp('Left');
             brick.MoveMotor(MotorPortLeft, manualspeed);
             brick.MoveMotor(MotorPortRight, -manualspeed);
-            % turnLeft(brick);
-           % angleCurr = brick.GyroAngle(GyroSensorPort);
-           % newAngle = angleCurr-86;
-           % brick.MoveMotor(MotorPortLeft, TurnSpeed);
-           % brick.MoveMotor(MotorPortRight, -TurnSpeed);
-           % while brick.GyroAngle(GyroSensorPort)>newAngle
-           %     pause(.1);
-           % end
-           % brick.StopMotor(MotorPortLeft, 'Brake');
-           % brick.StopMotor(MotorPortRight, 'Brake');
             
         case 'rightarrow'
             %disp('Right');
             brick.MoveMotor(MotorPortLeft, -manualspeed);
             brick.MoveMotor(MotorPortRight, manualspeed);
-            % turnRight(brick);
-            % angleCurr = brick.GyroAngle(GyroSensorPort);
-            % newAngle = angleCurr+86;
-            % brick.MoveMotor(MotorPortLeft, -TurnSpeed);
-            % brick.MoveMotor(MotorPortRight, TurnSpeed);
-            % while brick.GyroAngle(GyroSensorPort)<newAngle
-            %     pause(.1);
-            %     % angleCurr = brick.GyroAngle(GyroSensorPort);
-            % end
-        % brick.StopMotor(MotorPortLeft, 'Brake');
-        % brick.StopMotor(MotorPortRight, 'Brake');
             
         case 0
             %disp('No Key Press');
@@ -62,29 +41,4 @@ while 1
             break;
     end
 end
-
 CloseKeyboard();
-% function turnRight(brick)
-% angleCurr = brick.GyroAngle(GyroSensorPort);
-% newAngle = angleCurr+90;
-% while angleCurr<newAngle
-%     brick.MoveMotor(MotorPortLeft, -AutoSpeed);
-%     brick.MoveMotor(MotorPortRight, AutoSpeed);
-%     pause(.1);
-%     angleCurr = brick.GyroAngle(GyroSensorPort);
-% end
-% brick.StopMotor(MotorPortLeft, 'Brake');
-% brick.StopMotor(MotorPortRight, 'Brake');
-% end
-% function turnLeft(brick)
-% angleCurr = brick.GyroAngle(GyroSensorPort);
-% newAngle = angleCurr-90;
-% while angleCurr>newAngle
-%     brick.MoveMotor(MotorPortLeft, AutoSpeed);
-%     brick.MoveMotor(MotorPortRight, -AutoSpeed);
-%     pause(.1);
-%     angleCurr = brick.GyroAngle(GyroSensorPort);
-% brick.StopMotor(MotorPortLeft, 'Brake');
-% brick.StopMotor(MotorPortRight, 'Brake');
-% end  
-% end
